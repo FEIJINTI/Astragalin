162b632a199bf2a4c4d74393f7c2735ab3bf2a8c
FEIJINTI <83849113+FEIJINTI@users.noreply.github.com>
2023-06-13 16:58:37 +0800

    阈值分割背景

--- machine_learning.py
+++ machine_learning.py
@@ -11,8 +11,10 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pickle
 from sklearn.svm import SVC
-
-
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier
+from xgboost import XGBClassifier
 
 
 def DecisionTree(train_x, train_y, test_x, test_y, file_name, class_weight=None):
@@ -70,6 +72,87 @@ def RandomForest(train_x, train_y, test_x, test_y, file_name, class_weight=None)
     print('---------------------------------------------------------------------------------------------------')
     print('RF分类报告：' + str(classification_report(test_y, y_pred)))  # 生成一个小报告呀
     print('RF混淆矩阵：' + str(confusion_matrix(test_y, y_pred)))  # 这个也是，生成的矩阵的意思是有多少
+
+
+
+def AdaBoost(train_x, train_y, test_x, test_y, file_name, class_weight=None):
+    ada = AdaBoostClassifier(n_estimators= 2, random_state=65, class_weight=class_weight)
+    ada = ada.fit(train_x, train_y)
+    # 保存模型
+    with open(file_name, 'wb') as f:
+        pickle.dump(ada, f)
+
+    t1 = time.time()
+
+    y_pred = ada.predict(test_x)
+    print("预测时间：", time.time() - t1)
+    print("AdaBoost训练模型评分：" + str(accuracy_score(train_y, ada.predict(train_x))))
+    print("AdaBoost待测模型评分：" + str(accuracy_score(test_y, ada.predict(test_x))))
+    print('AdaBoost预测结果：' + str(y_pred))
+    print('---------------------------------------------------------------------------------------------------')
+    print('AdaBoost分类报告：' + str(classification_report(test_y, y_pred)))  # 生成一个小报告呀
+    print('AdaBoost混淆矩阵：' + str(confusion_matrix(test_y, y_pred)))  # 这个也是，生成的矩阵的意思是有多少
+    
+
+def GradientBoosting(train_x, train_y, test_x, test_y, file_name, class_weight=None):
+    gb = GradientBoostingClassifier(n_estimators= 2, random_state=65, class_weight=class_weight)
+    gb = gb.fit(train_x, train_y)
+    # 保存模型
+    with open(file_name, 'wb') as f:
+        pickle.dump(gb, f)
+
+    t1 = time.time()
+
+    y_pred = gb.predict(test_x)
+    print("预测时间：", time.time() - t1)
+    print("GradientBoosting训练模型评分：" + str(accuracy_score(train_y, gb.predict(train_x))))
+    print("GradientBoosting待测模型评分：" + str(accuracy_score(test_y, gb.predict(test_x))))
+    print('GradientBoosting预测结果：' + str(y_pred))
+    print('---------------------------------------------------------------------------------------------------')
+    print('GradientBoosting分类报告：' + str(classification_report(test_y, y_pred)))  # 生成一个小报告呀
+    print('GradientBoosting混淆矩阵：' + str(confusion_matrix(test_y, y_pred)))  # 这个也是，生成的矩阵的意思是有多少
+
+
+def XGBoost(train_x, train_y, test_x, test_y, file_name, class_weight=None):
+    xgb = XGBClassifier(n_estimators= 2, random_state=65, class_weight=class_weight)
+    xgb = xgb.fit(train_x, train_y)
+    # 保存模型
+    with open(file_name, 'wb') as f:
+        pickle.dump(xgb, f)
+
+    t1 = time.time()
+
+    y_pred = xgb.predict(test_x)
+    print("预测时间：", time.time() - t1)
+    print("XGBoost训练模型评分：" + str(accuracy_score(train_y, xgb.predict(train_x))))
+    print("XGBoost待测模型评分：" + str(accuracy_score(test_y, xgb.predict(test_x))))
+    print('XGBoost预测结果：' + str(y_pred))
+    print('---------------------------------------------------------------------------------------------------')
+    print('XGBoost分类报告：' + str(classification_report(test_y, y_pred)))  # 生成一个小报告呀
+    print('XGBoost混淆矩阵：' + str(confusion_matrix(test_y, y_pred)))  # 这个也是，生成的矩阵的意思是有多少
+
+
+def MLP(train_x, train_y, test_x, test_y, file_name, class_weight=None):
+    mlp = MLPClassifier(hidden_layer_sizes=(100, 100, 100), max_iter=1000, alpha=0.0001, solver='sgd',
+                        verbose=10, random_state=65, tol=0.000000001, class_weight=class_weight)
+    mlp = mlp.fit(train_x, train_y)
+    # 保存模型
+    with open(file_name, 'wb') as f:
+        pickle.dump(mlp, f)
+
+    t1 = time.time()
+
+    y_pred = mlp.predict(test_x)
+    print("预测时间：", time.time() - t1)
+    print("MLP训练模型评分：" + str(accuracy_score(train_y, mlp.predict(train_x))))
+    print("MLP待测模型评分：" + str(accuracy_score(test_y, mlp.predict(test_x))))
+    print('MLP预测结果：' + str(y_pred))
+    print('---------------------------------------------------------------------------------------------------')
+    print('MLP分类报告：' + str(classification_report(test_y, y_pred)))  # 生成一个小报告呀
+    print('MLP混淆矩阵：' + str(confusion_matrix(test_y, y_pred)))  # 这个也是，生成的矩阵的意思是有多少
+
+
+
 
 
 def predict(x, file_name):
